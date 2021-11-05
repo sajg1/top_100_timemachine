70552c06462ebac27cb9487d5418a863406aec74
Steve Gossip <stephengossip@gmail.com>
2021-11-05 18:38:53 +0000

    I changed the base url from billboard.com to officalcharts.com to get the british top 100. This meant changing the soup.find_all scrape.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import spotipy
 from pprint import pprint
 from spotipy.oauth2 import SpotifyOAuth
 
-BASE_URL = 'https://www.billboard.com/charts/hot-100'
+BASE_URL = 'https://www.officialcharts.com/charts/singles-chart/'
 CLIENT_ID = os.environ.get('CLIENT_ID_TOP100')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET_TOP100')
 
@@ -26,16 +26,17 @@ user_id = sp.current_user()['id']
 # Get response from user and scrape relevant top 100 song names from Billboard.com
 
 selected_year = input("Which year would you like to travel to? Type the date in this format YYYY-MM-DD: ")
+url_year = "".join(selected_year.split('-'))
 
-response = requests.get(f"{BASE_URL}/{selected_year}")
+response = requests.get(f"{BASE_URL}/{url_year}")
 
 selected_top_100 = response.text
 
 soup = BeautifulSoup(selected_top_100, 'html.parser')
 
-song_names_span = soup.find_all(name="span", class_="chart-element__information__song")
+song_names_div = soup.find_all(name="div", class_="title")
 
-song_names = [song.getText() for song in song_names_span]
+song_names = [song.getText().strip('\n') for song in song_names_div]
 
 # Get each songs track uri from the spotify API
 year = selected_year.split("-")[0]
@@ -47,9 +48,9 @@ for song in song_names:
         track_uri_list.append(track_uri)
     except IndexError:
         print("Not on spotify")
-
+print(len(track_uri_list))
 # create playlist
-new_playlist = sp.user_playlist_create(user=user_id, name=f"{selected_year} Billboard 100", public=False, collaborative=False, description="")
+new_playlist = sp.user_playlist_create(user=user_id, name=f"{selected_year} Official Singles Charts", public=False, collaborative=False, description="")
 new_playlist_id = new_playlist['id']
 print(new_playlist_id)
 
