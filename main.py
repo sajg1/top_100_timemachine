b6de0a5fbab2381be707d855078885a43eceb9bf
Steve Gossip <stephengossip@gmail.com>
2021-11-05 17:27:16 +0000

    use the list of song names to search the API against track name and year, in order to get a list of the tracks uri's.

--- main.py
+++ main.py
@@ -23,9 +23,7 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 
 user_id = sp.current_user()['id']
 
-
-
-
+# Get response from user and scrape relevant top 100 song names from Billboard.com
 
 selected_year = input("Which year would you like to travel to? Type the date in this format YYYY-MM-DD: ")
 
@@ -37,5 +35,18 @@ soup = BeautifulSoup(selected_top_100, 'html.parser')
 
 song_names_span = soup.find_all(name="span", class_="chart-element__information__song")
 
-
 song_names = [song.getText() for song in song_names_span]
+
+# Get each songs track uri from the spotify API
+year = selected_year.split("-")[0]
+track_uri_list = []
+for song in song_names:
+    result = sp.search(q=f"track: {song} year:{year}", type="track")
+    try:
+        track_uri = result['tracks']['items'][0]['uri']
+        track_uri_list.append(track_uri)
+    except IndexError:
+        print("Not on spotify")
+
+print(track_uri_list)
+
