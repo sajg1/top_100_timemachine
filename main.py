a245583f42d27027dc02c2194919a3c146948614
Steve Gossip <stephengossip@gmail.com>
2021-11-05 17:21:47 +0000

    created a spotify developer app called Billboard to Spotify and using the client_id and client secret, authenticated my project

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
-base_url = 'https://www.billboard.com/charts/hot-100'
+import os
+import spotipy
+from pprint import pprint
+from spotipy.oauth2 import SpotifyOAuth
+
+BASE_URL = 'https://www.billboard.com/charts/hot-100'
+CLIENT_ID = os.environ.get('CLIENT_ID_TOP100')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET_TOP100')
+
+# Authorisation for Spotify
+auth_manager = SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri="http://example.com",
+    cache_path='token.txt',
+    show_dialog=True,
+    scope="playlist-modify-private"
+)
+
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+user_id = sp.current_user()['id']
+
+
+
+
+
 selected_year = input("Which year would you like to travel to? Type the date in this format YYYY-MM-DD: ")
 
-response = requests.get(f"{base_url}/{selected_year}")
+response = requests.get(f"{BASE_URL}/{selected_year}")
 
 selected_top_100 = response.text
 
@@ -13,5 +39,3 @@ song_names_span = soup.find_all(name="span", class_="chart-element__information_
 
 
 song_names = [song.getText() for song in song_names_span]
-print(song_names)
-
